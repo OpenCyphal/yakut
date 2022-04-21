7d834deacf86d8ac2620c88e44327858b3872f96
Pavel Kirienko <pavel.kirienko@gmail.com>
2022-04-21 12:38:42 +0300

    Fix subscriber test

--- tests/cmd/subscribe.py
+++ tests/cmd/subscribe.py
@@ -15,12 +15,10 @@ def _unittest_subscribe(compiled_dsdl: Any) -> None:
         "UAVCAN__NODE__ID": "1234",
         "YAKUT_PATH": str(OUTPUT_DIR),
     }
-
     # No subjects specified.
     _, _, stderr = execute_cli("-vv", "sub", timeout=5.0, environment_variables=env)
     assert "nothing to do" in stderr.lower()
     assert "no subject" in stderr.lower()
-
     # Count zero.
     _, _, stderr = execute_cli(
         "-vv", "sub", "4444:uavcan.si.unit.force.Scalar", "--count=0", timeout=5.0, environment_variables=env
@@ -28,19 +26,32 @@ def _unittest_subscribe(compiled_dsdl: Any) -> None:
     assert "nothing to do" in stderr.lower()
     assert "count" in stderr.lower()
 
-    # Transport not specified.
-    result, _, stderr = execute_cli("sub", "4444:uavcan.si.unit.force.Scalar", timeout=5.0, ensure_success=False)
-    assert result != 0
-    assert "transport" in stderr.lower()
-
 
 def _unittest_dsdl_not_found() -> None:
-    env = {
-        "UAVCAN__LOOPBACK": "1",
-        "UAVCAN__NODE__ID": "1234",
-    }
     result, _, stderr = execute_cli(
-        "sub", "4444:uavcan.si.unit.force.Scalar", timeout=5.0, ensure_success=False, environment_variables=env
+        "sub",
+        "4444:uavcan.si.unit.force.Scalar",
+        timeout=5.0,
+        ensure_success=False,
+        environment_variables={
+            "UAVCAN__LOOPBACK": "1",
+            "UAVCAN__NODE__ID": "1234",
+        },
     )
     assert result != 0
     assert "yakut compile" in stderr.lower()
+
+
+def _unittest_transport_not_specified(compiled_dsdl: Any) -> None:
+    _ = compiled_dsdl
+    result, _, stderr = execute_cli(
+        "sub",
+        "4444:uavcan.si.unit.force.Scalar",
+        timeout=5.0,
+        ensure_success=False,
+        environment_variables={
+            "YAKUT_PATH": str(OUTPUT_DIR),
+        },
+    )
+    assert result != 0
+    assert "transport" in stderr.lower()

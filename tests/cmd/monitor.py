821ed9e55b3446b2feea17db60468c2cabd092b3
Pavel Kirienko <pavel.kirienko@gmail.com>
2024-01-23 12:33:10 +0200

    Robustify the monitor tests

--- tests/cmd/monitor.py
+++ tests/cmd/monitor.py
@@ -273,8 +273,8 @@ async def _run_nodes() -> None:
                     await reg_client_a.call(uavcan.register.List_1.Request(i % 11))
                 if i % 5 == 0:
                     await reg_client_b.call(uavcan.register.List_1.Request(i % 11))
-            await asyncio.sleep(0.1)
-    except (asyncio.TimeoutError, asyncio.CancelledError):  # pragma: no cover
+            await asyncio.sleep(0.2)
+    except (asyncio.TimeoutError, asyncio.CancelledError, GeneratorExit):  # pragma: no cover
         pass
     finally:
         _logger.info("STOPPING THE NODES...")
